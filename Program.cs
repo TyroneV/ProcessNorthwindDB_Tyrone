f7a98cddb1b4cc033deec6e8331e7016537c464e
TyroneV <tyroneveneracion@gmail.com>
2020-07-17 18:00:19 -0700

    Added using statements and delete now requires both firstname and lastname

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.Configuration;
 using System.Data.SqlClient;
 
-
 namespace ProcessNorthwindDB_Tyrone
 {
     class Program
@@ -14,12 +13,7 @@ namespace ProcessNorthwindDB_Tyrone
         private static string integratedSecurity = 
             ConfigurationManager.AppSettings.Get("IntegratedSecurity");
 
-        private static SqlConnection sqlConnection = new
-            SqlConnection($"Data Source={dataSource};" +
-            $"AttachDbFilename={fileName};" +
-            $"Integrated Security={integratedSecurity}");
-
-        private static void OpenConnection()
+        private static void OpenConnection(SqlConnection sqlConnection)
         {
             try
             {
@@ -36,34 +30,47 @@ namespace ProcessNorthwindDB_Tyrone
 
         static void Main(string[] args)
         {
-            OpenConnection();
-            Dml dml = new Dml();
+            using (SqlConnection sqlConnection = new
+            SqlConnection($"Data Source={dataSource};" +
+            $"AttachDbFilename={fileName};" +
+            $"Integrated Security={integratedSecurity}"))
+            {
+                OpenConnection(sqlConnection);
+                Dml dml = new Dml();
 
-            //Display Original Data Rows
-            Console.WriteLine("\n");
-            Console.WriteLine("Display Rows Before Insertion:");
-            dml.SelectRows(sqlConnection);
-            Console.WriteLine("\n");
-            Console.WriteLine("Insert Row operation:***");
-            dml.InsertRows(sqlConnection);
-            //Display Rows Before Insertion
-            Console.WriteLine("\n");
-            Console.WriteLine("Display Rows After Insertion:");
-            dml.SelectRows(sqlConnection);
-            //Update Rows
-            Console.WriteLine("\n");
-            Console.WriteLine("Perform Update***");
-            dml.UpdateRows(sqlConnection);
-            //Display Rows Before Insertion
-            Console.WriteLine("\n");
-            Console.WriteLine("Display Rows After Update:");
-            dml.SelectRows(sqlConnection);
-            //Clean up with delete of all inserted rows
-            Console.WriteLine("\n");
-            Console.WriteLine("Clean Up By Deleting Inserted Rows***");
-            dml.DeleteRows(sqlConnection);
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadLine();
+                //Display Original Data Rows
+                Console.WriteLine("\n");
+                Console.WriteLine("Display Rows Before Insertion:");
+                dml.SelectRows(sqlConnection);
+                Console.WriteLine("\n");
+                Console.WriteLine("Insert Row operation:***");
+                dml.InsertRows(sqlConnection);
+                //Display Rows Before Insertion
+                Console.WriteLine("\n");
+                Console.WriteLine("Display Rows After Insertion:");
+                dml.SelectRows(sqlConnection);
+                //Update Rows
+                Console.WriteLine("\n");
+                Console.WriteLine("Perform Update***");
+                dml.UpdateRows(sqlConnection);
+                //Display Rows Before Insertion
+                Console.WriteLine("\n");
+                Console.WriteLine("Display Rows After Update:");
+                dml.SelectRows(sqlConnection);
+                //Clean up with delete of all inserted rows
+                Console.WriteLine("\n");
+                Console.WriteLine("Clean Up By Deleting Inserted Rows***");
+                dml.DeleteRows(sqlConnection);
+                //Display Rows After Cleanup
+                Console.WriteLine("\n");
+                Console.WriteLine("Display Rows After Cleanup:");
+                dml.SelectRows(sqlConnection); 
+                // Close Connection
+                sqlConnection.Close();
+                Console.WriteLine("Connection Closed");
+                Console.WriteLine("\nPress <ENTER> to quit...");
+                Console.ReadKey();
+            }
         }
     }
 }
